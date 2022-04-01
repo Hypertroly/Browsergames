51733d8d4101a2c8554f3b51a18873928de440c8
Tiago Nunes <43524597+Hypertroly@users.noreply.github.com>
2022-04-01 15:24:12 -0300

    Página HTML com javascript com código em Java

---
+++ webapp_bgames/src/main/java/ps2/restapp/GameBasic.java
@@ -0,0 +1,51 @@
+package ps2.restapp;
+
+public class GameBasic {
+
+	private String nome;
+	private String URL;
+	private String URLVideo;
+	private String descricao;
+	private String categoria_nomeCategoria;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getURL() {
+		return URL;
+	}
+
+	public void setURL(String URL) {
+		this.URL = URL;
+	}
+
+	public String getURLVideo() {
+		return URLVideo;
+	}
+
+	public void setURLVideo(String URLVideo) {
+		this.URLVideo = URLVideo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCategoria_nomeCategoria() {
+		return categoria_nomeCategoria;
+	}
+
+	public void setCategoria_nomeCategoria(String categoria_nomeCategoria) {
+		this.categoria_nomeCategoria = categoria_nomeCategoria;
+	}
+
+}
