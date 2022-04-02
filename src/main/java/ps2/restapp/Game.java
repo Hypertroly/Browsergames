658237d57feb980d666d0e41f9f24a398f007882
Hypertroly <tiago2000nb@hotmail.com>
2022-04-01 21:42:41 -0300

    Deploy

--- src/main/java/ps2/restapp/Game.java
+++ src/main/java/ps2/restapp/Game.java
@@ -2,8 +2,6 @@ package ps2.restapp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,7 +10,6 @@ import javax.persistence.Table;
 public class Game {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "nome")
 	private String nome;
 
